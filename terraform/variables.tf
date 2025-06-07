a61f2758029e0742e54de49b42b1c523fd35fb44
takuya-sangil <jayloveswonyoung@gmail.com>
2025-06-08 02:14:25 +0800

    updated tf files

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,6 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-
 variable "aws_region" {
-    default = "us-east-1"
+    description = "AWS Region"
+    default     = "us-east-1"
 }
 
 variable "instance_type" {
@@ -11,10 +9,8 @@ variable "instance_type" {
 
 variable "key_name" {
     description = "AWS key_name"
-    default     = string
 }
 
 variable "my_ip" {
     description = "for ssh"
-    default = string
 }
